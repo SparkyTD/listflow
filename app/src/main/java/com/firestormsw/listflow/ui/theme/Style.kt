36023e0e3f5655c3f064aac31baea5ea94049839
SparkyTD <45818400+SparkyTD@users.noreply.github.com>
2025-03-14 09:03:16 +0200

    Added required fonts and style constants

---
+++ app/src/main/java/com/firestormsw/listflow/ui/theme/Style.kt
@@ -0,0 +1,13 @@
+package com.firestormsw.listflow.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object CornerRadius {
+    val Small = 10.dp
+    val Medium = 16.dp
+}
+
+object BorderThickness {
+    val Medium = 3.dp
+    val Thick = 4.dp
+}
--- app/src/main/java/com/firestormsw/listflow/ui/theme/Type.kt
+++ app/src/main/java/com/firestormsw/listflow/ui/theme/Type.kt
@@ -1,29 +1,55 @@
 package com.firestormsw.listflow.ui.theme
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.firestormsw.listflow.R
+
+val DefaultFontFamily = FontFamily(
+    Font(R.font.lato_thin, FontWeight.Thin),
+    Font(R.font.lato_thin_italic, FontWeight.Thin, FontStyle.Italic),
+    Font(R.font.lato_light, FontWeight.Light),
+    Font(R.font.lato_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.lato_regular, FontWeight.Normal),
+    Font(R.font.lato_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.lato_bold, FontWeight.Bold),
+    Font(R.font.lato_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.lato_black, FontWeight.Black),
+    Font(R.font.lato_black_italic, FontWeight.Black, FontStyle.Italic),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = DefaultFontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        letterSpacing = 1.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = DefaultFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 1.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = DefaultFontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
+    /*labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
@@ -32,3 +58,11 @@ val Typography = Typography(
     )
     */
 )
+
+@Preview
+@Composable
+fun TypographyPreview() {
+    Column {
+        Text("When the quick brown fox jumped over the lazy dog", style = Typography.bodyLarge, color = TextPrimary)
+    }
+}
