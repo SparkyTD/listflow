988fefad83085187356bc0a174d08895518b3b97
SparkyTD <45818400+SparkyTD@users.noreply.github.com>
2025-03-16 11:45:02 +0200

    Fixed EditItemSheet increase / decrease button scaling consistency

--- app/src/main/java/com/firestormsw/listflow/ui/sheets/EditItemSheet.kt
+++ app/src/main/java/com/firestormsw/listflow/ui/sheets/EditItemSheet.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -21,7 +22,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
@@ -144,7 +144,7 @@ fun EditItemSheet(
                         .height(60.dp)
                         .padding(top = 4.dp)
                 ) {
-                    Icon(Remove, contentDescription = null, modifier = Modifier.scale(4f))
+                    Icon(Remove, contentDescription = null, modifier = Modifier.requiredSize(24.dp))
                 }
 
                 // Increase quantity
@@ -162,7 +162,7 @@ fun EditItemSheet(
                         .height(60.dp)
                         .padding(top = 4.dp)
                 ) {
-                    Icon(Add, contentDescription = null, modifier = Modifier.scale(4f))
+                    Icon(Add, contentDescription = null, modifier = Modifier.requiredSize(24.dp))
                 }
             }
 
